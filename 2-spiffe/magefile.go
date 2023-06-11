4be40d34a13d744751f2b5b69f01a62bdfec8bd9
Amim Knabben <aknabben@vmware.com>
2023-06-11 18:54:22 -0300

    App specification

--- 2-spiffe/magefile.go
+++ 2-spiffe/magefile.go
@@ -7,22 +7,39 @@ import (
 	"fmt"
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"time"
 )
 
 type K8S mg.Namespace
 
-const SPEC_PATH = "./specs/install.yaml"
+const (
+	SPEC_PATH = "./specs/install.yaml"
+	SPEC_APPS = "./specs/app.yaml"
+)
 
 // Install installs resources into the cluster
 func (K8S) Install() error {
+	if err := sh.RunV("kind", "delete", "cluster"); err != nil {
+		return err
+	}
+	if err := sh.RunV("kind", "create", "cluster"); err != nil {
+		return err
+	}
+
 	if err := sh.RunV("kubectl", "apply", "-f", SPEC_PATH); err != nil {
 		return err
 	}
-	return nil
-}
 
-// InstallSpecs create a client/server serviceaccount and permissions for them on SPIRE
-func (K8S) InstallSpecs() error {
+	for {
+		// wait for server run, if pod does not exist keep trying.
+		args := []string{"wait", "--for=condition=Ready", "-n", "spire", "pod", "-l", "app=spire-server", "--timeout", "300s"}
+		if err := sh.RunV("kubectl", args...); err == nil {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+
+	// create the SPIRE node attestation
 	if err := sh.RunV("kubectl",
 		"exec", "-n", "spire",
 		"spire-server-0", "--",
@@ -34,6 +51,8 @@ func (K8S) InstallSpecs() error {
 		"-node"); err != nil {
 		return err
 	}
+
+	// create client and server wl attestation
 	for _, n := range []string{"client", "server"} {
 		if err := sh.RunV("kubectl", "create", "serviceaccount", n); err != nil {
 			return err
@@ -50,12 +69,22 @@ func (K8S) InstallSpecs() error {
 			return err
 		}
 	}
+
+	// Install client/server deployment
+	if err := sh.RunV("kubectl", "apply", "-f", SPEC_APPS); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // Delete cleans up resources from cluster
 func (K8S) Delete() error {
 	if err := sh.RunV("kubectl", "delete", "-f", SPEC_PATH); err != nil {
+		return err
+	}
+
+	if err := sh.RunV("kubectl", "apply", "-f", SPEC_APPS); err != nil {
 		return err
 	}
 
@@ -67,10 +96,10 @@ func (K8S) Delete() error {
 
 // ContainerClient build the SPIFFE gRPC client and push to registry
 func (K8S) ContainerClient() error {
-	if err := sh.RunV("docker", "build", "-f", "code/client/Dockerfile", "-t", "knabben:spiffe-client", "code/client"); err != nil {
+	if err := sh.RunV("docker", "build", "-f", "code/client/Dockerfile", "-t", "knabben/spiffe-client", "code/client"); err != nil {
 		return err
 	}
-	if err := sh.RunV("docker", "push", "knabben:spiffe-client"); err != nil {
+	if err := sh.RunV("docker", "push", "knabben/spiffe-client"); err != nil {
 		return err
 	}
 	return nil
@@ -78,10 +107,10 @@ func (K8S) ContainerClient() error {
 
 // ContainerServer build the SPIFFE gRPC server and push to registry
 func (K8S) ContainerServer() error {
-	if err := sh.RunV("docker", "build", "-f", "code/server/Dockerfile", "-t", "knabben:spiffe-server", "code/server"); err != nil {
+	if err := sh.RunV("docker", "build", "-f", "code/server/Dockerfile", "-t", "knabben/spiffe-server", "code/server"); err != nil {
 		return err
 	}
-	if err := sh.RunV("docker", "push", "knabben:spiffe-server"); err != nil {
+	if err := sh.RunV("docker", "push", "knabben/spiffe-server"); err != nil {
 		return err
 	}
 	return nil
